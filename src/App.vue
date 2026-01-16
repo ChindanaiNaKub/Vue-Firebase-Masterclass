208978be7236d278c786aab411465db35f49eabf
ChindanaiNaKub <chindanai_jaiman@cmu.ac.th>
2026-01-16 17:30:41 +0700

    v-else

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,10 @@
 <template>
   <div>
     <div v-if="isCheck">
-      แสดงคำนี้ออกมา
+      แสดงคำนี้ออกมา เมื่อ isCheck เป็น True
+    </div>
+    <div v-else>
+      แสดงคำนี้ออกมา เมื่อ isCheck เป็น False
     </div>
     <button @click="toggleCheck()"> กดเพื่อเปิด</button>
   </div>
